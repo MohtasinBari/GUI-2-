cda6c4110ad9fd0c5827332a859b635ae651c5cb
Mohtasin Bari <24-58305-2@student.aiub.edu>
2025-06-03 22:24:34 +0600

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import GUI.*;
+
+public class Start {
+    public static void main(String[] args) {
+        RailwayReservationSystem rrs = new RailwayReservationSystem();
+    }
+}
